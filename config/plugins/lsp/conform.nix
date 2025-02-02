338f2ab7751f2ff18ad6aac5ecfbbd469d333cd1
Evan Azevedo <me@evanazevedo.com>
2025-02-02 10:29:43 -0700

    adding formatters back

--- config/plugins/lsp/conform.nix
+++ config/plugins/lsp/conform.nix
@@ -1,5 +1,5 @@
 {
-  ...
+  lib, pkgs, ...
 }: {
   config = {
     extraConfigLuaPre =
@@ -115,6 +115,41 @@
           ];
           json = ["jq"];
           "_" = ["trim_whitespace"];
+        };
+        formatters = {
+          black = {
+            command = "${lib.getExe pkgs.black}";
+          };
+          isort = {
+            command = "${lib.getExe pkgs.isort}";
+          };
+          nixfmt-rfc-style = {
+            command = "${lib.getExe pkgs.nixfmt-rfc-style}";
+          };
+          alejandra = {
+            command = "${lib.getExe pkgs.alejandra}";
+          };
+          jq = {
+            command = "${lib.getExe pkgs.jq}";
+          };
+          prettierd = {
+            command = "${lib.getExe pkgs.prettierd}";
+          };
+          stylua = {
+            command = "${lib.getExe pkgs.stylua}";
+          };
+          shellcheck = {
+            command = "${lib.getExe pkgs.shellcheck}";
+          };
+          shfmt = {
+            command = "${lib.getExe pkgs.shfmt}";
+          };
+          shellharden = {
+            command = "${lib.getExe pkgs.shellharden}";
+          };
+          bicep = {
+            command = "${lib.getExe pkgs.bicep}";
+          };
         };
       };
     };
