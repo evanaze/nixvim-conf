8aeb9e3891f10f80ed433cdac572cac621f18936
Evan Azevedo <me@evanazevdo.com>
2025-02-03 15:40:50 -0700

    python lsp

--- config/plugins/lsp/lsp.nix
+++ config/plugins/lsp/lsp.nix
@@ -2,7 +2,6 @@
   plugins = {
     lsp-lines = {enable = true;};
     lsp-format = {enable = true;};
-    helm = {enable = true;};
     lsp = {
       enable = true;
       inlayHints = true;
@@ -12,45 +11,11 @@
         lua_ls = {enable = true;};
         nil_ls = {enable = true;};
         marksman = {enable = true;};
-        pyright = {enable = true;};
+        pylsp = {enable = true;};
         gopls = {enable = true;};
         terraformls = {enable = true;};
         ts_ls = {enable = true;};
-        ansiblels = {enable = true;};
         jsonls = {enable = true;};
-        helm_ls = {
-          enable = true;
-          extraOptions = {
-            settings = {
-              "helm-ls" = {
-                yamlls = {
-                  path = "${pkgs.yaml-language-server}/bin/yaml-language-server";
-                };
-              };
-            };
-          };
-        };
-        yamlls = {
-          enable = true;
-          extraOptions = {
-            settings = {
-              yaml = {
-                schemas = {
-                  kubernetes = "'*.yaml";
-                  "http://json.schemastore.org/github-workflow" = ".github/workflows/*";
-                  "http://json.schemastore.org/github-action" = ".github/action.{yml,yaml}";
-                  "http://json.schemastore.org/ansible-stable-2.9" = "roles/tasks/*.{yml,yaml}";
-                  "http://json.schemastore.org/kustomization" = "kustomization.{yml,yaml}";
-                  "http://json.schemastore.org/ansible-playbook" = "*play*.{yml,yaml}";
-                  "http://json.schemastore.org/chart" = "Chart.{yml,yaml}";
-                  "https://json.schemastore.org/dependabot-v2" = ".github/dependabot.{yml,yaml}";
-                  "https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json" = "*docker-compose*.{yml,yaml}";
-                  "https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json" = "*flow*.{yml,yaml}";
-                };
-              };
-            };
-          };
-        };
       };
 
       keymaps = {
@@ -106,9 +71,6 @@
       };
     };
   };
-  extraPlugins = with pkgs.vimPlugins; [
-    ansible-vim
-  ];
 
   extraConfigLua = ''
     local _border = "rounded"
